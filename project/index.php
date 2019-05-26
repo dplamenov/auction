71485b938b1eb07efd4b559f9acdefa971b3048c
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-26 15:30:02 +0300

    create login form

--- project/index.php
+++ project/index.php
@@ -1,2 +1,12 @@
 <?php
 require 'config.php';
+if(isset($_POST['login'])){
+    //todo check user
+    $_SESSION['islogged'] = true;
+    header('Location: index.php');
+}
+if(@$_SESSION['islogged'] === true){
+    include 'logged.php';
+}else{
+    include 'login_form.php';
+}
