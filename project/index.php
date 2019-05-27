dd849ac1b6314a22094909b7f667ef0cb2235c72
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-27 16:08:11 +0300

    log in users

--- project/index.php
+++ project/index.php
@@ -1,14 +1,16 @@
 <?php
 require 'config.php';
-if(isset($_POST['login'])){
+if (isset($_POST['login'])) {
     $email = mysqli_real_escape_string($dbc, $_POST['email']);
     $password = mysqli_real_escape_string($dbc, $_POST['password']);
     $users = mysqli_query($dbc, "SELECT * FROM `users` WHERE email = '$email' and password = '$password'");
-    $_SESSION['islogged'] = true;
-    header('Location: index.php');
+    if (mysqli_num_rows($users) == 1) {
+        $_SESSION['islogged'] = true;
+        header('Location: index.php');
+    }
 }
-if(@$_SESSION['islogged'] === true){
+if (@$_SESSION['islogged'] === true) {
     include 'logged.php';
-}else{
+} else {
     include 'login_form.php';
 }
