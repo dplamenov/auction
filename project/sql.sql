c6aa55a9a4e107c64430472d8ad9f83d33840417
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-27 17:23:17 +0300

    add create sql for table products

--- project/sql.sql
+++ project/sql.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS `users`
   `password` VARCHAR(80) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `products`
+(
+  `product_id`    INT         NOT NULL AUTO_INCREMENT,
+  `user_id`       INT         NOT NULL,
+  `product_title` VARCHAR(50) NOT NULL,
+  `start_price`   INT         NOT NULL,
+  `start`         VARCHAR(40) NOT NULL,
+  `end`           VARCHAR(40) NOT NULL,
+  `description`   TEXT        NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE = InnoDB;
