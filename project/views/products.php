398bc789edfaaeab74adf088133fd041c4b81f0b
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-28 17:56:12 +0300

    display product title

--- project/views/products.php
+++ project/views/products.php
@@ -8,10 +8,11 @@
 <a href="logout.php">Log out</a>
 <a href="addproduct.php">Add product</a>
 <table>
-    <tr><th>Image</th></tr>
+    <tr><th>Image</th><th>Product Title</th></tr>
 <?php
 while ($product = mysqli_fetch_assoc($products)){
-    echo '<tr><td><img src='.$product["image_url"].' style="width:64px"/></td></tr>';
+    echo '<tr><td><img src='.$product["image_url"].' style="width:64px"/></td>
+<td style="padding: 0; vertical-align: top">'.$product['product_title'].'</td></tr>';
 }
 ?>
 </table>
