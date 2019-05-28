332a87432cf352ce8af3c485d57f162f6eb39dcc
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-28 16:42:21 +0300

    use view

--- project/logged.php
+++ project/logged.php
@@ -1,11 +1,8 @@
-<a href="logout.php">Log out</a>
-<a href="addproduct.php">Add product</a>
+
 <?php
 if($_SESSION['islogged'] !== true){
     header('Location: index.php');
     exit;
 }
 $products = mysqli_query($dbc, "SELECT * FROM products");
-while ($product = mysqli_fetch_assoc($products)){
-
-}
+view('products');
--- project/views/products.php
+++ project/views/products.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Products</title>
+</head>
+<body>
+<a href="logout.php">Log out</a>
+<a href="addproduct.php">Add product</a>
 <?php
+
+?>
+</body>
+</html>
