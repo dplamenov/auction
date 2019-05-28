d6c41d1b2441cdbaa652f9bed571c1b4fb3e7fc9
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-05-28 16:36:17 +0300

    table products

--- project/sql.sql
+++ project/sql.sql
@@ -8,13 +8,14 @@ CREATE TABLE IF NOT EXISTS `users`
 
 CREATE TABLE IF NOT EXISTS `products`
 (
-  `product_id`    INT         NOT NULL AUTO_INCREMENT,
-  `user_id`       INT         NOT NULL,
-  `product_title` VARCHAR(50) NOT NULL,
-  `start_price`   INT         NOT NULL,
-  `start`         VARCHAR(40) NOT NULL,
-  `end`           VARCHAR(40) NOT NULL,
-  `description`   TEXT        NOT NULL,
-  `price`         INT         NOT NULL,
+  `product_id`    INT          NOT NULL AUTO_INCREMENT,
+  `user_id`       INT          NOT NULL,
+  `product_title` VARCHAR(50)  NOT NULL,
+  `start_price`   INT          NOT NULL,
+  `start`         VARCHAR(40)  NOT NULL,
+  `end`           VARCHAR(40)  NOT NULL,
+  `description`   TEXT         NOT NULL,
+  `price`         INT          NOT NULL,
+  `image_url`     VARCHAR(200) NOT NULL,
   PRIMARY KEY (`product_id`)
 ) ENGINE = InnoDB;
