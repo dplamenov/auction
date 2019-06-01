fab9da61d7fefd5a5abf8e44a97de7e1893f2a40
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-01 11:26:25 +0300

    display product price

--- project/views/products.php
+++ project/views/products.php
@@ -8,11 +8,13 @@
 <a href="logout.php">Log out</a>
 <a href="addproduct.php">Add product</a>
 <table>
-    <tr><th>Image</th><th>Product Title</th></tr>
+    <tr><th>Image</th><th>Product Title</th><th>Start price</th><th>Current price</th></tr>
 <?php
 while ($product = mysqli_fetch_assoc($products)){
+    echo '<pre>' . print_r($product, true) . '</pre>';
     echo '<tr><td><img src='.$product["image_url"].' style="width:64px"/></td>
-<td style="padding: 0; vertical-align: top">'.$product['product_title'].'</td></tr>';
+<td style="padding: 0; vertical-align: top">'.$product['product_title'].'</td><td style="padding: 0; vertical-align: top">'.$product['start_price'].'</td>
+<td style="padding: 0; vertical-align: top">'.$product['price'].'</td></tr>';
 }
 ?>
 </table>
