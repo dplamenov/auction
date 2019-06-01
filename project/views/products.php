4deb764b02f6fc381d1d383baa7693af93468455
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-06-01 12:04:47 +0300

    display all products info

--- project/views/products.php
+++ project/views/products.php
@@ -8,13 +8,13 @@
 <a href="logout.php">Log out</a>
 <a href="addproduct.php">Add product</a>
 <table>
-    <tr><th>Image</th><th>Product Title</th><th>Start price</th><th>Current price</th></tr>
+    <tr><th>Image</th><th>Product Title</th><th>Start price</th><th>Current price</th><th>Auction start</th><th>Auction end</th></tr>
 <?php
 while ($product = mysqli_fetch_assoc($products)){
-    echo '<pre>' . print_r($product, true) . '</pre>';
     echo '<tr><td><img src='.$product["image_url"].' style="width:64px"/></td>
 <td style="padding: 0; vertical-align: top">'.$product['product_title'].'</td><td style="padding: 0; vertical-align: top">'.$product['start_price'].'</td>
-<td style="padding: 0; vertical-align: top">'.$product['price'].'</td></tr>';
+<td style="padding: 0; vertical-align: top">'.$product['price'].'</td><td style="padding: 0; vertical-align: top">'.date('d.m.Y H:i:s', intval($product['start'])).'</td>
+<td style="padding: 0; vertical-align: top">'.date('d.m.Y H:i:s', intval($product['end'])).'</td></tr>';
 }
 ?>
 </table>
